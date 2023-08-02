f7474a340b6835182c8e4f6d52e2def0204e5ee6
tuannka <tuan.nguyenkieuanh@gmail.com>
2023-08-02 21:28:26 +0700

    Update

--- quick-start/src/main/java/com/example/quickstart/QuickStartApplication.java
+++ quick-start/src/main/java/com/example/quickstart/QuickStartApplication.java
@@ -1,4 +1,5 @@
 package com.example.quickstart;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class QuickStartApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(QuickStartApplication.class, args);
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(QuickStartApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 }
